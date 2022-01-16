b72556a047cc6031ef7f03ed0ebeeddc5cab13f7
jrs264 <j.r.rutgers@student.vu.nl>
2022-01-16 22:49:19 +0100

    correction on T and S computation

--- local_linear_estimation_cai.py
+++ local_linear_estimation_cai.py
@@ -4,7 +4,9 @@ import pickle
 from time_varying_coefficient_estimation import transform_data
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+
 np.random.seed(2022)
+
 
 def kernel_function(u):
     """
@@ -13,7 +15,7 @@ def kernel_function(u):
     """
 
     if abs(u) < 1:
-        res = (3 / 4) *(1 - u ** 2)
+        res = (3 / 4) * (1 - u ** 2)
     else:
         res = 0
     return res
@@ -23,27 +25,27 @@ def compute_S(X, k, time_steps, tau, h):
     n = len(time_steps)
     sum = 0
     for i in range(n):
-        weight = (time_steps[i]**k)*(kernel_function((time_steps[i]-tau)/h)/h)
-        sum += np.matmul(X[:,i].reshape(-1, 1), X[:,i].reshape(-1, 1).T)*weight
+        weight = ((time_steps[i] - tau) ** k) * (kernel_function((time_steps[i] - tau) / h) / h)
+        sum += np.matmul(X[:, i].reshape(-1, 1), X[:, i].reshape(-1, 1).T) * weight
 
-    S = sum/n
+    S = sum / n
 
     return S
+
 
 def compute_T(X, Y, k, time_steps, tau, h):
     n = len(time_steps)
     sum = 0
     for i in range(n):
-        weight = (time_steps[i]**k)*(kernel_function((time_steps[i]-tau)/h)/h)
-        sum += X[:,i].reshape(-1,1)*weight*Y[i]
+        weight = ((time_steps[i] - tau) ** k) * (kernel_function((time_steps[i] - tau) / h) / h)
+        sum += X[:, i].reshape(-1, 1) * weight * Y[i]
 
-    T = sum/n
+    T = sum / n
 
     return T
 
 
 def compute_theta(X, Y, time_steps, tau, h):
-    X = np.vstack((np.ones((1, len(X))), X.reshape(1, -1)))
     S0 = compute_S(X, 0, time_steps, tau, h)
     S1 = compute_S(X, 1, time_steps, tau, h)
     S2 = compute_S(X, 2, time_steps, tau, h)
@@ -57,10 +59,8 @@ def compute_theta(X, Y, time_steps, tau, h):
 
     theta = np.matmul(np.linalg.inv(S), T)
 
-    if theta[1] > 100:
-        print('debug')
-
     return theta
+
 
 def main():
     source = 'reproduction_vs_index_Japan.pkl'
@@ -68,21 +68,32 @@ def main():
         raw = pickle.load(f)
 
     data = raw.dropna()  # removes nans
-    data = data.iloc[0:458] # bad slicing, works for now
+    data = data.iloc[0:458]  # bad slicing, works for now
     y_log, X_log = transform_data(data['Rt'], data['StringencyIndex'])
     y_log, X_log = np.array(y_log), np.array(X_log)
+    X = np.vstack((np.ones((1, len(X_log))), X_log.reshape(1, -1)))
     steps = np.random.uniform(low=0, high=1, size=(1000,))
     steps.sort()
     time_steps = np.array(data['time_index'] / len(data))
-    h = 0.05
+    h = 0.09
     theta_all = np.empty((len(steps), 2))
     for i, step in enumerate(tqdm(steps)):
-        result = compute_theta(X_log, y_log, time_steps, step, h)
+        result = compute_theta(X, y_log, time_steps, step, h)
         theta_all[i] = result[:2].T
 
-    print(theta_all[:,1])
+    print(theta_all[:, 1])
 
-    plt.plot(theta_all[:,1])
+    plt.plot(theta_all[:, 1])
+    plt.title('beta 1')
     plt.show()
+
+    y_pred = np.dot(X.T, theta_all.T)
+    plt.title('y pred')
+    y_pred = np.diagonal(y_pred)
+
+    plt.plot(y_pred)
+    plt.show()
+
+
 if __name__ == '__main__':
     main()
