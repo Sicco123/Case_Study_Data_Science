c55435cdf7e38cc5d1343557997bfb9aace69afa
jrs264 <j.r.rutgers@student.vu.nl>
2022-01-17 17:21:13 +0100

    code formatting

--- bandwith_selection.py
+++ bandwith_selection.py
@@ -15,17 +15,19 @@ from local_linear_estimation_cai import local_linear_estimation
 from local_linear_estimation_cai import kernel_function
 import math
 
+
 def compute_W(time_steps, t, h):
     t_array = time_steps - t
     w_i = lambda t: kernel_function(t / h)
     vfunc = np.vectorize(w_i, otypes=[float])
     diagonal = vfunc(t_array)
-    diagonal = diagonal/h
+    diagonal = diagonal / h
 
     W = np.zeros((len(time_steps), len(time_steps)))
     np.fill_diagonal(W, diagonal)
 
     return W
+
 
 def compute_X_tilde(X, time_steps, t):
     t_array = time_steps - t
@@ -34,10 +36,11 @@ def compute_X_tilde(X, time_steps, t):
 
     return X_tilde
 
+
 def compute_A(X, time_steps, t, h):
     p = X.shape[1]
     I = np.identity(p)
-    zero_matrix = np.zeros((p,p))
+    zero_matrix = np.zeros((p, p))
     I_weight = np.hstack([I, zero_matrix])
     X_tilde = compute_X_tilde(X, time_steps, t)
     W = compute_W(time_steps, t, h)
@@ -48,9 +51,10 @@ def compute_A(X, time_steps, t, h):
 
     return A
 
+
 def compute_n_h(X, time_steps, h):
     S_star = []
-    for i,t in enumerate(time_steps):
+    for i, t in enumerate(time_steps):
         A = compute_A(X, time_steps, t, h)
         S_star.append(np.matmul(A.T, X[i]))
 
@@ -59,24 +63,24 @@ def compute_n_h(X, time_steps, h):
 
     return n_h
 
+
 def compute_aic(y, X, theta_estimate, time_steps, h):
     n = len(y)
     y_pred = np.dot(X.T, theta_estimate.T)
     y_pred = np.diagonal(y_pred)
-    sigma_sq = ((y-y_pred)**2).mean()
+    sigma_sq = ((y - y_pred) ** 2).mean()
     n_h = compute_n_h(X.T, time_steps, h)
 
-    aic = math.log(sigma_sq) + (2*(n_h+1)/(n-n_h-2))
+    aic = math.log(sigma_sq) + (2 * (n_h + 1) / (n - n_h - 2))
 
     return aic
+
 
 def main():
     bandwidth_values = [0.15, 0.2, 0.1, 0.05]
     y, X, time_steps = get_and_prepare_data()
     steps = np.random.uniform(low=0, high=1, size=(1000,))
     steps.sort()
-
-    # compute_S_star(X.T, time_steps, 0.15)
 
     for bw in bandwidth_values:
         theta_estimate = local_linear_estimation(y, X, time_steps, steps, bw)
