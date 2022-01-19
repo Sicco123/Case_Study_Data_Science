58f85fa6b5b71034f7d8b9cd5c62e9842d8b95a1
Sicco123 <s.h.kooiker@student.vu.nl>
2022-01-19 10:47:01 +0100

    refactoring of documents

---
+++ Junk/OLS_regressions.py
@@ -0,0 +1,34 @@
+import pickle
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import math
+import pandas as pd
+#%% Import data
+with open('../reproduction_vs_index_Japan.pkl', 'rb') as f:
+    raw = pickle.load(f)
+
+
+#%% remove rows with NA 
+data = raw.dropna()
+
+# Regress Rt for three indices 
+
+for index in ["StringencyIndex",'GovernmentResponseIndex', "ContainmentHealthIndex"]:
+    
+    # OLS regression
+    X = np.log(data[index])
+    X = sm.add_constant(X)
+    Y = np.log(data['Rt'])
+    print(len(Y))
+    print(len(X))
+    model = sm.OLS(Y,X)
+    results = model.fit()
+    print(results.params)
+
+    # Plot 
+    plt.figure()
+    plt.scatter(X[index], Y, [2] * len(Y) )
+    plt.plot(X[index], results.params[1] * X[index] + results.params[0])
+
+    plt.show()
