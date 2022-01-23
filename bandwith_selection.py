Correct computation of H matrix

bandwith_selection.py
@@ -13,74 +13,84 @@ import numpy as np
 from local_linear_estimation_cai import get_and_prepare_data
 from local_linear_estimation_cai import local_linear_estimation
 from local_linear_estimation_cai import kernel_function
+from local_linear_estimation_cai import compute_S_k
+from local_linear_estimation_cai import compute_weight
 import math
 import pickle
 from tqdm import tqdm
 
+def compute_T_prime_K(X, time_steps, tau, h, k):
+    n = len(time_steps)
+    weight = compute_weight(k, time_steps, tau, h)
+    W = np.zeros((n, n))
+    np.fill_diagonal(W, weight)
+    T_k = (1/n) * np.matmul(X, W)
 
-def compute_W(time_steps, t, h):
-    t_array = time_steps - t
-    w_i = lambda t: kernel_function(t / h)
-    vfunc = np.vectorize(w_i, otypes=[float])
-    diagonal = vfunc(t_array)
-    diagonal = diagonal / h
-
-    W = np.zeros((len(time_steps), len(time_steps)))
-    np.fill_diagonal(W, diagonal)
-
-    return W
+    return T_k
 
 
-def compute_X_tilde(X, time_steps, t):
-    t_array = time_steps - t
-    X_time = X * t_array.reshape(-1, 1)
-    X_tilde = np.hstack([X, X_time])
+def compute_T_prime(X, time_steps, tau, h):
+    T0 = compute_T_prime_K(X, time_steps, tau, h, 0)
+    T1 = compute_T_prime_K(X, time_steps, tau, h, 1)
 
-    return X_tilde
+    T_prime = np.vstack([T0, T1])
+
+    return T_prime
+
+def compute_S(X, time_steps, tau, h):
+    S0 = compute_S_k(X, 0, time_steps, tau, h)
+    S1 = compute_S_k(X, 1, time_steps, tau, h)
+    S2 = compute_S_k(X, 2, time_steps, tau, h)
+    S_top = np.hstack((S0, S1.T))
+    S_bottom = np.hstack((S1, S2))
+    S = np.vstack((S_top, S_bottom))
+    S = np.linalg.inv(S)
+
+    return S
+
+def compute_H_row(x, X, identity, time_steps, tau, h):
+    S = compute_S(X, time_steps, tau, h)
+    T_prime = compute_T_prime(X, time_steps, tau, h)
+    # H_row = np.matmul(np.matmul(x, identity), np.matmul(S, T_prime))
+    H_row = np.matmul(x, np.matmul(S, T_prime))
 
 
-def compute_A(X, time_steps, t, h):
-    p = X.shape[1]
-    I = np.identity(p)
-    zero_matrix = np.zeros((p, p))
-    I_weight = np.hstack([I, zero_matrix])
-    X_tilde = compute_X_tilde(X, time_steps, t)
-    W = compute_W(time_steps, t, h)
-    A1 = np.linalg.inv(np.matmul(X_tilde.T, np.matmul(W, X_tilde)))
-    A2 = np.matmul(X_tilde.T, W)
-
-    A = np.matmul(I_weight, np.matmul(A1, A2))
-
-    return A
-
+    return H_row
 
 def compute_n_h(X, time_steps, h):
-    S_star = []
-    for i, t in enumerate(time_steps):
-        A = compute_A(X, time_steps, t, h)
-        S_star.append(np.matmul(A.T, X[i]))
+    p, n = X.shape
+    X_tilde = np.hstack([X.T, np.zeros((n, p))])  #TODO: maybe replace with Z
+    identity = np.zeros((2*p, 2*p))
+    identity_diagonal = np.array([1]*p + [0]*p)
+    np.fill_diagonal(identity, identity_diagonal)
 
-    S_star = np.stack(S_star)
-    n_h = np.trace(S_star)
+    H_matrix = []
+    for i, tau in enumerate(time_steps):
+        H_row = compute_H_row(X_tilde[i].reshape(1,-1), X, identity, time_steps, tau, h)
+        H_matrix.append(H_row)
+
+    H_matrix = np.stack(H_matrix).reshape(n, n)
+    n_h = np.trace(H_matrix)
+
+    return n_h, H_matrix
 
 
-    return n_h, S_star
-
-
-def compute_aic(y, X, theta_estimate, time_steps, h):
+def compute_aic(y, X, y_pred, time_steps, h):
     n = len(y)
-    # y_pred = np.dot(X.T, theta_estimate.T)
-    # y_pred = np.diagonal(y_pred)
-    # sigma_sq = ((y - y_pred) ** 2).mean()
-    n_h, S_star = compute_n_h(X.T, time_steps, h)
+
+    n_h, H_matrix = compute_n_h(X, time_steps, h)
+
+    # middle = np.matmul((np.identity(n) - H_matrix).T, (np.identity(n) - H_matrix))
+    # RSS = np.matmul(y.reshape(-1, 1).T, np.matmul(middle, y.reshape(-1, 1))) * 1/n
     #
-    # aic = math.log(sigma_sq) + (2 * (n_h + 1) / (n - n_h - 2))
+    # aic = math.log(RSS) + (2 * (1 + n_h) / (n - n_h - 2))
 
-
-    middle = np.matmul((np.identity(n)-S_star).T, (np.identity(n)-S_star))
-    RSS = np.matmul(y.reshape(-1, 1).T, np.matmul(middle, y.reshape(-1,1)))
-
-    aic = math.log(RSS) + (2*(1 + n_h) / (n - n_h - 2))
+    sigma_sq = ((y - y_pred) ** 2).mean()
+    print(f'trace = {n_h}')
+    print(f'sigma sq = {sigma_sq}')
+    print('Penalty =', (2 * (n_h + 1) / (n - n_h - 2)))
+    aic = math.log(sigma_sq) + (2 * (n_h + 1) / (n - n_h - 2))
+    # aic = math.log(sigma_sq)
 
     return aic
 
@@ -88,18 +98,21 @@ def compute_aic(y, X, theta_estimate, time_steps, h):
 def main():
     bandwidth_values = np.linspace(0.01, 1, 100)
     y, X, time_steps = get_and_prepare_data()
-    steps = np.random.uniform(low=0, high=1, size=(1000,))
+    # steps = np.random.uniform(low=0, high=1, size=(1000,)
+    steps = time_steps
     steps.sort()
     results = []
 
     for bw in tqdm(bandwidth_values):
         theta_estimate = local_linear_estimation(y, X, time_steps, steps, bw)
-        aic = compute_aic(y, X, theta_estimate, time_steps, bw)
+        y_pred = np.diagonal(np.matmul(X.T, theta_estimate.T))
+        aic = compute_aic(y, X, y_pred, time_steps, bw)
         results.append((bw, aic))
         print(f'AIC for bw of {bw} = {aic}')
-
+    #
     with open('bw_selection_2007.pkl', 'wb') as f:
         pickle.dump(results, f)
+
 
 if __name__ == '__main__':
     main()

local_linear_estimation_cai.py
@@ -5,7 +5,7 @@ from Junk.time_varying_coefficient_estimation import transform_data
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-np.random.seed(2022)
+# np.random.seed(2022)
 
 def get_and_prepare_data():
     source = 'reproduction_vs_index_Japan.pkl'
@@ -27,7 +27,7 @@ def kernel_function(u):
                   u: float
     """
 
-    if abs(u) < 1:
+    if abs(u) <= 1:
         res = (3 / 4) * (1 - u ** 2)
     else:
         res = 0
@@ -44,10 +44,13 @@ def compute_weight(k, time_steps, tau, h):
     return weight
 
 
-def compute_S(X, k, time_steps, tau, h):
+def compute_S_k(X, k, time_steps, tau, h):
     n = len(time_steps)
     weight = compute_weight(k, time_steps, tau, h)
-    S = (1 / n) * np.matmul(X, X.T * weight.reshape(-1, 1))
+    W = np.zeros((n, n))
+    np.fill_diagonal(W, weight)
+    S = (1/n) * np.matmul(X, np.matmul(W, X.T))
+
 
     return S
 
@@ -61,9 +64,9 @@ def compute_T(X, Y, k, time_steps, tau, h):
 
 
 def compute_theta(X, Y, time_steps, tau, h):
-    S0 = compute_S(X, 0, time_steps, tau, h)
-    S1 = compute_S(X, 1, time_steps, tau, h)
-    S2 = compute_S(X, 2, time_steps, tau, h)
+    S0 = compute_S_k(X, 0, time_steps, tau, h)
+    S1 = compute_S_k(X, 1, time_steps, tau, h)
+    S2 = compute_S_k(X, 2, time_steps, tau, h)
     T0 = compute_T(X, Y, 0, time_steps, tau, h)
     T1 = compute_T(X, Y, 1, time_steps, tau, h)
 
@@ -86,13 +89,15 @@ def local_linear_estimation(y, X, time_steps, steps, h):
 
 def main():
     y, X, time_steps = get_and_prepare_data()
-    steps = np.random.uniform(low=0, high=1, size=(1000,))
+    # steps = np.random.uniform(low=0, high=1, size=(1000,))
+    steps = time_steps
     steps.sort()
 
-    h = 0.15
+    h = 0.06
 
     theta_estimate = local_linear_estimation(y, X, time_steps, steps, h)
-    plt.plot(theta_estimate[:, 1])
+    print(theta_estimate[:-2, 1])
+    plt.plot(theta_estimate[:-2, 1])
     plt.title('beta 1')
     plt.show()
 

simulate_example1_cai.py
@@ -13,8 +13,9 @@ import pickle
 import numpy as np
 import math as ms
 import matplotlib.pyplot as plt
-from local_linear_estimation_cai import kernel_function, compute_S, compute_T, compute_theta, local_linear_estimation
-from bandwith_selection import compute_W, compute_X_tilde, compute_A, compute_n_h, compute_aic
+from local_linear_estimation_cai import kernel_function, compute_S_k, compute_T, compute_theta, local_linear_estimation
+from bandwith_selection import compute_aic
+from tqdm import tqdm
 
 import pandas as pd
 
@@ -75,16 +76,19 @@ def get_bandwidth_aic(y, X, time_steps, steps):
             time_steps: array of size (n,)
                 steps: array of size ?
     """
-    bandwidth_values = [0.01 + i * 0.0025 for i in range(4)]
-    
+    # bandwidth_values = [0.01 + i * 0.0025 for i in range(4)]
+    bandwidth_values = np.linspace(0.01, 0.1, 10)
+
     # Lists to store the theta estimates and aics  
     lst_theta_estimate = []
     lst_aic = []
     
-    for bw in bandwidth_values:
+    for bw in tqdm(bandwidth_values):
         theta_estimate = local_linear_estimation(y, X, time_steps, steps, bw)
+        y_pred = np.diagonal(np.matmul(X.T, theta_estimate.T))
         lst_theta_estimate += [theta_estimate]
-        aic = compute_aic(y, X, theta_estimate, time_steps, bw)
+        aic = compute_aic(y, X, y_pred, time_steps, bw)
+        print(bw, aic)
         lst_aic += [[bw, aic]]
         
     # h_opt = optimal bandwidth
@@ -122,7 +126,7 @@ def do_LL_for_specific_bw(number, bw):
     do_LL_for_specific_bw: Returns a specific
                 
     """
-    store_theta  = []
+    store_theta = []
     store_timesteps = []
     store_made = []
     
@@ -143,16 +147,17 @@ def MADE(beta_hat, beta):
 
 def main():
     
-    curve_estimates_1, lst_timesteps, lst_made = do_LL_for_specific_bw(50, 0.275)
-    #curve_estimates_2, h_store = perform_multiple_LL(500)
+    # curve_estimates_1, lst_timesteps, lst_made = do_LL_for_specific_bw(50, 0.275)
+    curve_estimates_2, h_store = perform_multiple_LL(1)
+
+    print(h_store)
     
-    
-    plt.figure()
-    for curve, time_steps in zip(curve_estimates_1, lst_timesteps):
-        plt.plot(time_steps, curve[:,1], color = "green")
-        plt.plot(time_steps, vbeta_1(time_steps), color = "red") 
-        
-    return lst_made
+    # plt.figure()
+    # for curve, time_steps in zip(curve_estimates_1, lst_timesteps):
+    #     plt.plot(time_steps, curve[:,1], color = "green")
+    #     plt.plot(time_steps, vbeta_1(time_steps), color = "red")
+    #
+    # return lst_made
 
 if __name__ == '__main__':
     start = datetime.now()