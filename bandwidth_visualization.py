10ed5d9db2384ff2ce71aa2398403c419fcfeb06
jrs264 <j.r.rutgers@student.vu.nl>
2022-01-23 14:45:06 +0100

    bandwidth visualization

--- bandwidth_visualization.py
+++ bandwidth_visualization.py
@@ -2,22 +2,17 @@ import matplotlib.pyplot as plt
 import pickle
 import numpy as np
 
-with open('bw_selection_2002.pkl', 'rb') as f:
-    results = pickle.load(f)
-
-plt.scatter(*zip(*results), s=0.5)
-plt.show()
-
-bw_values = np.array(list(map(list, zip(*results)))).T
-argmin = np.argmin(bw_values[:,1])
-optimal_bw_row = bw_values[argmin]
-print(f'Optimal bandwidth for 2007 based selection is {round(optimal_bw_row[0], 2)} with AIC of {optimal_bw_row[1]}')
 
 with open('bw_selection_2007.pkl', 'rb') as f:
     results = pickle.load(f)
 
-plt.scatter(*zip(*results), s=0.5)
-plt.show()
+fig, ax = plt.subplots()
+
+ax.scatter(*zip(*results), s=2)
+ax.set_xlabel('Bandwidth')
+ax.set_ylabel('AIC')
+fig.show()
+fig.savefig('Figures/AIC_bw.pdf')
 
 bw_values = np.array(list(map(list, zip(*results)))).T
 argmin = np.argmin(bw_values[:,1])
